da0292ef3bfd9c5d63edd3a1acd3e108058791dd
SaharCarmel <sahar.carmel@levitection.com>
2019-11-14 20:21:44 +0200

    Added basic classes structure

--- pcamera/utils.py
+++ pcamera/utils.py
@@ -1,11 +1,39 @@
+import cv2
+import matplotlib.pyplot as plt
+
 class Image():
     def __init__(self, imageFilePath, boxFilePath):
         self.imageFilePath = imageFilePath
         self.boxFilePath = boxFilePath
+        self.image = 0
     
-    def plotImageWithBox(self, imageFilePath, boxFilePath):
+    def loadImage(self, showFlag):
+        self.image = cv2.imread(self.imageFilePath)
+        if showFlag:
+            plt.imshow(image)
+            plt.show()
+
+    def loadBoxPoints(self):
+        """loadBoxPoints loads the boxes cooridantes into a struct
+        """        
+        pass
+
+    def plotImageWithBox(self):
+        """plotImageWithBox plots the image with the annoted boxes from files.
+        
+        """        
         pass
 
     def imageHistogram(self):
         pass
+
+
+class Images():
+    def __init__(self):
+        self.images = 0
+
+    def loadImagesFromFolder(self,folderPath):
+        pass
     
+    def totalHistogram(self):
+        pass
