ae0970317ae9e530427a4f1d11997b7d13100d59
SaharCarmel <sahar.carmel@levitection.com>
2019-11-14 19:53:58 +0200

    Added init Image class

---
+++ pcamera/utils.py
@@ -0,0 +1,11 @@
+class Image():
+    def __init__(self, imageFilePath, boxFilePath):
+        self.imageFilePath = imageFilePath
+        self.boxFilePath = boxFilePath
+    
+    def plotImageWithBox(self, imageFilePath, boxFilePath):
+        pass
+
+    def imageHistogram(self):
+        pass
+    
