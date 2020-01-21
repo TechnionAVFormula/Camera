275dfbaae3744218dfc1d9439165f7b36249dcce
evorkin <evorkin@gmail.com>
2020-01-21 22:05:46 +0200

    minor change

--- pcamera/utils.py
+++ pcamera/utils.py
@@ -144,8 +144,8 @@ class Image():
         boundRectB = []
         areasB = np.array([cv2.contourArea(countour) for countour in contoursB])
         for i, c in enumerate(contoursB):
-            contours_polyB.append(cv2.approxPolyDP(c, 3, True))
-            if areasB[i] > 30 and areasB[i] < 140:
+            contours_polyB.append(cv2.approxPolyDP(c, 1, False))
+            if areasB[i] > 20 and areasB[i] < 140:
                boundRectB.append(cv2.boundingRect(contours_polyB[i]))
                self.idan_helper.append(cv2.boundingRect(contours_polyB[i]))
         for i in range(len(boundRectB)):
@@ -278,6 +278,44 @@ class Image():
 
         cv2.waitKey(0)
 
+    def maskTemplate(self):
+        img = self.image.copy()
+        mask_temp = cv2.imread('blue_cone.png')
+        mask = mask_temp.copy()
+
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        mask = cv2.cvtColor(mask_temp, cv2.COLOR_BGR2GRAY)
+
+        w, h = mask.shape[: : -1]
+
+        # All the 6 methods for comparison in a list
+        methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
+                   'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+
+        for meth in methods:
+           method = eval(meth)
+
+            # Apply template Matching
+           res = cv2.matchTemplate(img,mask,method)
+           min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+            # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+           if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+               top_left = min_loc
+           else:
+               top_left = max_loc
+               bottom_right = (top_left[0] + w, top_left[1] + h)
+            #   bottom_right = (top_left[0] + 5, top_left[1] + 5)
+
+           cv2.rectangle(img,top_left, bottom_right, 255, 2)
+
+           plt.subplot(121),plt.imshow(res,cmap = 'gray')
+           plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+           plt.subplot(122),plt.imshow(img,cmap = 'gray')
+           plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+           plt.suptitle(meth)
+
+           plt.show()
 
 class Images():
     def __init__(self):
@@ -289,13 +327,16 @@ class Images():
     def totalHistogram(self):
         pass
 
-path = "yolo_cones\data\Combo_img\in5_0001"
+
+
+path = "yolo_cones\data\Combo_img\in8_0010"
 image = Image(path)
-image.imageMask()
+image.maskTemplate()
+# image.imageMask()
 # image.imageShowRectHSV(minimumRectangleArea=15)
-image.imageHSV_ratio()
-print("check struct  ")
-print(image.idan_helper)
+# image.imageHSV_ratio()
+# print("check struct  ")
+# print(image.idan_helper)
 
 images = []
 image2 = pilimage.open('yolo_cones\data\Combo_img\in5_0001.png')
